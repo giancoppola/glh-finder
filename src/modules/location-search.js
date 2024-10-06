35803e49ac98fece530b0bab6f235573dce7a447
giancoppola <44214851+giancoppola@users.noreply.github.com>
2024-10-06 22:29:38 +0100

    -

--- src/modules/location-search.js
+++ src/modules/location-search.js
@@ -1,34 +1,13 @@
 import { jsx as _jsx, Fragment as _Fragment } from "react/jsx-runtime";
-import { useRef, useEffect, useState } from "react";
-import { useMapsLibrary } from '@vis.gl/react-google-maps';
-// This is an example of the classic "Place Autocomplete" widget.
-// https://developers.google.com/maps/documentation/javascript/place-autocomplete
-export var PlaceAutocompleteClassic = function (_a) {
-    var onPlaceSelect = _a.onPlaceSelect;
-    var _b = useState(null), placeAutocomplete = _b[0], setPlaceAutocomplete = _b[1];
-    var inputRef = useRef(null);
-    var places = useMapsLibrary('places');
-    useEffect(function () {
-        if (!places || !inputRef.current)
-            return;
-        var options = {
-            fields: ['geometry', 'name', 'formatted_address']
-        };
-        setPlaceAutocomplete(new places.Autocomplete(inputRef.current, options));
-    }, [places]);
-    useEffect(function () {
-        if (!placeAutocomplete)
-            return;
-        placeAutocomplete.addListener('place_changed', function () {
-            onPlaceSelect(placeAutocomplete.getPlace());
-        });
-    }, [onPlaceSelect, placeAutocomplete]);
-    return (_jsx("div", { className: "autocomplete-container", children: _jsx("input", { ref: inputRef }) }));
-};
+import { GOOGLE_MAPS_API_KEY } from "../globals.js";
+import { useState } from "react";
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 export var LocationSearch = function () {
     var _a = useState(""), searchKeyword = _a[0], setSearchKeyword = _a[1];
     var HandleLocationSearch = function (place) {
         console.log(place);
     };
-    return (_jsx(_Fragment, { children: _jsx(PlaceAutocompleteClassic, { onPlaceSelect: HandleLocationSearch }) }));
+    return (_jsx(_Fragment, { children: _jsx(GooglePlacesAutocomplete, { apiKey: GOOGLE_MAPS_API_KEY, selectProps: {
+                onChange: HandleLocationSearch,
+            } }) }));
 };
