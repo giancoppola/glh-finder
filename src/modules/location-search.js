a9d90fc727fc22dce87727656dfb11d3e55ab730
giancoppola <44214851+giancoppola@users.noreply.github.com>
2024-10-06 22:47:34 +0100

    got lat lng from chosen place

--- src/modules/location-search.js
+++ src/modules/location-search.js
@@ -1,11 +1,15 @@
 import { jsx as _jsx, Fragment as _Fragment } from "react/jsx-runtime";
 import { GOOGLE_MAPS_API_KEY } from "../globals.js";
 import { useState } from "react";
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import GooglePlacesAutocomplete, { geocodeByPlaceId, getLatLng } from 'react-google-places-autocomplete';
 export var LocationSearch = function () {
     var _a = useState(""), searchKeyword = _a[0], setSearchKeyword = _a[1];
-    var HandleLocationSearch = function (place) {
-        console.log(place);
+    var HandleLocationSearch = function (result) {
+        console.log(result);
+        geocodeByPlaceId(result.value.place_id)
+            .then(function (data) { return getLatLng(data[0]); })
+            .then(function (latlng) { return console.log(latlng); })
+            .catch(function (error) { return console.error(error); });
     };
     return (_jsx(_Fragment, { children: _jsx(GooglePlacesAutocomplete, { apiKey: GOOGLE_MAPS_API_KEY, selectProps: {
                 onChange: HandleLocationSearch,
--- src/modules/location-search.tsx
+++ src/modules/location-search.tsx
@@ -3,12 +3,21 @@ import { GOOGLE_MAPS_API_KEY } from "../globals.js";
 import { Dispatch, useRef, useEffect, useState } from "react";
 import { Typography, Theme, createTheme, responsiveFontSizes, ThemeProvider, TextField, Box, Input, FormControl, OutlinedInput } from "@mui/material";
 
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import GooglePlacesAutocomplete, { geocodeByPlaceId, getLatLng } from 'react-google-places-autocomplete';
+
+interface SearchResult {
+  label: string,
+  value: google.maps.places.PlaceResult,
+}
 
 export const LocationSearch = () => {
     const [searchKeyword, setSearchKeyword]: [string, Dispatch<string>] = useState<string>("");
-    const HandleLocationSearch = (place: google.maps.places.PlaceResult | null) => {
-        console.log(place);
+    const HandleLocationSearch = (result: SearchResult) => {
+        console.log(result);
+        geocodeByPlaceId(result!.value.place_id!)
+        .then(data => getLatLng(data[0]))
+        .then(latlng => console.log(latlng))
+        .catch(error => console.error(error));
     }
     return (
         <>
