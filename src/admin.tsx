526787b7c2e333676c9a980ed5a4c2ee8d38cf7b
Gian <giancoppola@hotmail.co.uk>
2024-11-20 10:35:30 +0000

    admin conditional

--- src/admin.tsx
+++ src/admin.tsx
@@ -10,17 +10,14 @@ theme = responsiveFontSizes(theme);
 
 const AdminPage = () => {
     const [isVerified, setIsVerified]: [boolean, Dispatch<boolean>] = useState(false);
-    const [username, setUsername]: [string, Dispatch<string>] = useState("");
-    const [password, setPassword]: [string, Dispatch<string>] = useState("");
-    const FormValidation = () => {
-        console.log('form validation happens here');
-    }
     return (
         <StrictMode>
             <ThemeProvider theme={theme}>
                 <Box component="section" display="flex" flexDirection="column" justifyContent="center"
                     alignItems="center" width='100%' height='100%' gap='2rem' maxWidth='1200px' margin='auto'>
-                    <AdminLoginForm setIsVerified={setIsVerified}/>
+                    { !isVerified &&
+                        <AdminLoginForm setIsVerified={setIsVerified}/>
+                    }
                 </Box>
             </ThemeProvider>
         </StrictMode>
